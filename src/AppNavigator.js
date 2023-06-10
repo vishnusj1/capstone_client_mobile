fa5207a89f3c9c3c230c3412355fb19c064a40df
vishnusj <vishnusj12@gmail.com>
2023-06-10 15:15:59 +1000

    Add: Route to Tab Navigation

--- src/AppNavigator.js
+++ src/AppNavigator.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+import TabNavigator from './TabNavigation';
 import Login from './Login';
 import Register from './Register';
-import WatchListScreen from './WatchListScreen';
+
 
 const Stack = createStackNavigator();
 
 function AppNavigator() {
   return (
-    <Stack.Navigator initialRouteName="Login">
+    <Stack.Navigator initialRouteName="Home">
       <Stack.Screen name="Login" component={Login} options={{ headerShown: false }}/>
       <Stack.Screen name="Register" component={Register} options={{ headerShown: false }}/>
-      <Stack.Screen name="WatchListScreen" component={WatchListScreen} options={{ headerShown: false }}/>
+      <Stack.Screen name="Home" component={TabNavigator}  options={{ headerShown: false }}/>
     </Stack.Navigator>
   );
 }
