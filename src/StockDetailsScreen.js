6792aefd39f7798c26f1b23ec05bbf925ac3c195
vishnusj <vishnusj12@gmail.com>
2023-06-11 13:07:36 +1000

    Add: Stock Screen

---
+++ src/StockDetailsScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function StockDetailsScreen() {
+  return (
+    <View>
+      <Text>Stock Details</Text>
+    </View>
+  );
+}
