4894f390a92aee2ffd594cc81622110b3f953bcf
vishnusj <vishnusj12@gmail.com>
2023-06-11 16:10:51 +1000

    Add: Route Stock Details Screen

---
+++ src/MainStackNavigator.js
@@ -0,0 +1,16 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import TabNavigator from './TabNavigation';
+import StockDetailsScreen from './StockDetailsScreen';
+
+const MainStack = createStackNavigator();
+
+function MainStackNavigator() {
+  return (
+    <MainStack.Navigator initialRouteName="Home">
+      <MainStack.Screen name="Home" component={TabNavigator} options={{ headerShown: false }} />
+      <MainStack.Screen name="StockDetails" component={StockDetailsScreen} options={{ headerShown: false}} />
+    </MainStack.Navigator>
+  );
+}
+
+export default MainStackNavigator;
--- src/StockDetailsScreen.js
+++ src/StockDetailsScreen.js
@@ -1,10 +1,37 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { View, Text } from "react-native";
+import axios from "axios";
+import { ALPHA_VANTAGE_API_KEY } from "@env";
 
-export default function StockDetailsScreen() {
+export default function StockDetailsScreen({ route, navigation }) {
+  const { symbol } = route.params;
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    const fetchStockData = async () => {
+      try {
+        const response = await fetch(
+          `https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=${symbol}&apikey=${ALPHA_VANTAGE_API_KEY}`
+        ); // - Url to call the alpha vantage api
+        const data = await response.json();
+        const timeSeries = data["Monthly Time Series"];
+        const prices = Object.values(timeSeries).map((value) =>
+          parseFloat(value["4. close"])
+        );
+        const dates = Object.keys(timeSeries).map((date) => new Date(date));
+        setData({ prices, dates });
+        navigation.setOptions({ title: symbol });
+      } catch (error) {
+        console.error("Failed to fetch stock data", error);
+      }
+    };
+
+    fetchStockData();
+  }, [symbol]);
+
   return (
     <View>
-      <Text>Stock Details</Text>
+      <Text>Stock Details: {symbol}</Text>
     </View>
   );
 }
